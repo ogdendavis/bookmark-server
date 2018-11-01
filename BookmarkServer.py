249b733d830067f4cf691b2a310d64a857074c04
Lucas Ogden-Davis <lucas@ogdendavis.com>
2018-11-01 14:06:57 -0400

    Add ThreadHTTPServer for concurrency

--- BookmarkServer.py
+++ BookmarkServer.py
@@ -45,6 +45,8 @@ import os
 import http.server
 import requests
 from urllib.parse import unquote, parse_qs
+import threading
+from socketserver import ThreadingMixIn
 
 memory = {}
 
@@ -66,6 +68,9 @@ form = '''<!DOCTYPE html>
 {}
 </pre>
 '''
+
+class ThreadHTTPServer(ThreadingMixIn, http.server.HTTPServer):
+    "This is an HTTPServer that supports thread-based concurrency."
 
 
 def CheckURI(uri, timeout=5):
@@ -147,5 +152,5 @@ class Shortener(http.server.BaseHTTPRequestHandler):
 if __name__ == '__main__':
     port = int(os.environ.get('PORT', 8000))
     server_address = ('', port)
-    httpd = http.server.HTTPServer(server_address, Shortener)
+    httpd = http.server.ThreadHTTPServer(server_address, Shortener)
     httpd.serve_forever()
